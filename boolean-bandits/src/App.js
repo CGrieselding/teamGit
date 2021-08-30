857d46fc64eab7a29ca9326412e54e445b6804db
Cameron Grieselding <cameron@Camerons-MacBook-Pro.local>
2021-08-30 15:40:10 -0400

    React App Creation v2

--- boolean-bandits/src/App.js
+++ boolean-bandits/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/boostrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
     </div>
   );
 }
