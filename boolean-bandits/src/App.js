cca930084b5d454081155b50bcda5835fc259b65
Sophie Russell <Passion4webdev@outlook.com>
2021-08-31 09:52:20 -0400

    pull

--- boolean-bandits/src/App.js
+++ boolean-bandits/src/App.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
+import AppLocation from './components/Location/AppLocation';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
 import Nasa from './components/NASA';
 
 function App() {
@@ -12,30 +12,17 @@ function App() {
     <div className="App">
       <h1>Boolean Bandits!</h1>
 			<Nasa lon={ lon } lat={ lat }/>
-
-// import GetLocation from './components/Location/GeoLocation';
-import AppLocation from './components/Location/AppLocation';
-
-
-
-
-function App() {
-  
- 
-
-  
-  return (
-    <div className="App">
-    <h1>Boolean Bandits!</h1>
-    <AppLocation/>
-    <div>
-    
-
-    </div>
-
-    </div>
-    
+      <AppLocation/>
+      </div>
+       
     );
   }
 
 export default App;
+
+
+
+
+
+
+
