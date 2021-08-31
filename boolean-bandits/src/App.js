3c4b95a44593ef2be68a1220d3119c7d084894d7
bscott125 <bwinfieldscott125@gmail.com>
2021-08-31 09:00:13 -0400

    API push

--- boolean-bandits/src/App.js
+++ boolean-bandits/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Nasa from './components/NASA';
 
 function App() {
+	const lon = -111.0937
+	const lat = 39.3210
   return (
     <div className="App">
       <h1>Boolean Bandits!</h1>
+			<Nasa lon={ lon } lat={ lat }/>
     </div>
   );
 }
