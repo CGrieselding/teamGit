6cbb6edadc31d90468e6f2e303992d7e14e536be
Sophie Russell <Passion4webdev@outlook.com>
2021-08-31 12:33:43 -0400

    API weather

--- boolean-bandits/src/App.js
+++ boolean-bandits/src/App.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import AppLocation from './components/Location/AppLocation';
+// import GetLocation from './components/Location/GeoLocation';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Nasa from './components/NASA';
+import APIweather from './components/Weather/API weather';
 
 function App() {
 	const lon = -111.0937
@@ -13,6 +15,8 @@ function App() {
       <h1>Boolean Bandits!</h1>
 			<Nasa lon={ lon } lat={ lat }/>
       <AppLocation/>
+      <APIweather />
+      {/* <TicketMaster /> */}
       </div>
        
     );
--- boolean-bandits/src/components/Weather/API weather.js
+++ boolean-bandits/src/components/Weather/API weather.js
@@ -1,49 +1,25 @@
 import React, { useState } from 'react';
 
-const baseURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=50&appid=e51267c357017ca3a21eada5bfb90157'
+const key = 'e51267c357017ca3a21eada5bfb90157'
+const baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=50&appid=${key}`
 
-// const key = 'e51267c357017ca3a21eada5bfb90157' 
-
-
-const fetchResults = () => {
-  let url = `${baseURL}?api-key=${key}&page=${pageNumber}&q=${search}`;
-  url = startDate ? url + `&begin_date=${startDate}` : url;
-  url = endDate ? url + `&end_date=${endDate}` : url;
-
-  const APIweather = () => {
-    const [search, setSearch] = useState('')
-    const [startDate, setStartDate] = useState('')
-    const [endDate, setEndDate] = useState('')
-    const [pageNumber, setPageNumber] = useState('')
-    const [results, setResults] = useState('')
-  }
-
-  fetch(url)
+const APIweather = () => {
+  const [weather, setWeather] = useState([]);
+  fetch(baseURL)
     .then(res => res.json())
-    .then(data => setResults(data.response.docs))
-    .catch(err => console.log(err));
-};
-const handlesSubmit = (event) => {
-  event.preventDefault();
-  this.fetchResults();
-}
+        .then(data => {
+          setWeather(data.list[0].main.temp)
+      console.log(data.list[0].main.temp)
+    })
 
-return (
-  <div className="main">
-    <div className="mainDiv">
-      <form onSubmit={(e) => handlesSubmit(e)}>
-        <span>Enter a single serach time (required) : </span>
-        <input type="text" name="search" onChange={(e) => setSearch(e.target.value)} required />
-        <br />
-        <span>Enter a start date: </span>
-        <input type="date" name="startDate" pattern="[0-9]{8}" onChange={(e) => setSearch(e.target.value)} />
-        <br />
-        <span>Enter a end Date: </span>
-        <input type="date" name="endDate" pattern="[0-9]{8}" onChange={(e) => setSearch(e.target.value)} />
-        <br />
-        <button className="submit">Submit search</button>
-      </form>
+    .catch(err => console.log(err));
+
+  return (
+    <div className="main">
+    <p>tempF{((weather-273.15)*1.8)+32}</p>
+    <p>tempC{weather-273.15}</p>    
+    <button id="button">toggle</button>
     </div>
-  </div>
-);
+  );
+}
 export default APIweather;
