7b3e01a805921e145bd8e9da6153ce132572e90f
Sophie Russell <Passion4webdev@outlook.com>
2021-08-31 09:00:34 -0400

    API weathers

---
+++ boolean-bandits/src/components/Weather/WeatherResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import APIweather from './API weather';
+import App from './App';
+
+const WeatherResults = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default APIweather;
